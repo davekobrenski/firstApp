46716a474873461aef862a2ea2047847b9e8a406
dkobrenski <dave@bbmdesigns.com>
2018-10-25 08:45:04 -0400

    ionic error handling setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,42 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { Camera } from '@ionic-native/camera';
 import { WebView } from '@ionic-native/ionic-webview/ngx';
 
+import { Pro } from '@ionic/pro';
+import { Injectable, Injector } from '@angular/core';
+//import { IonicErrorHandler } from 'ionic-angular';
+
+Pro.init('f46a074a', {
+  appVersion: '0.0.2'
+});
+
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import {SecondPage} from "../pages/second/second";
 import {ThirdPage} from "../pages/third/third";
+
+
+//see instructions at https://ionicframework.com/docs/pro/basics/getting-started/
+//for setting up Ionic Pro error handling etc
+@Injectable()
+export class MyErrorHandler implements ErrorHandler {
+  ionicErrorHandler: IonicErrorHandler;
+
+  constructor(injector: Injector) {
+    try {
+      this.ionicErrorHandler = injector.get(IonicErrorHandler);
+    } catch(e) {
+      // Unable to get the IonicErrorHandler provider, ensure
+      // IonicErrorHandler has been added to the providers list below
+    }
+  }
+
+  handleError(err: any): void {
+    Pro.monitoring.handleNewError(err);
+    // Remove this if you want to disable Ionic's auto exception handling
+    // in development mode.
+    this.ionicErrorHandler && this.ionicErrorHandler.handleError(err);
+  }
+}
 
 @NgModule({
   declarations: [
@@ -34,7 +66,9 @@ import {ThirdPage} from "../pages/third/third";
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     Camera,
-    WebView
+    WebView,
+    IonicErrorHandler,
+    [{ provide: ErrorHandler, useClass: MyErrorHandler }]
   ]
 })
 export class AppModule {}
